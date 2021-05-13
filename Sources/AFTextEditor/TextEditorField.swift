f99b0d97676a2d4600ffd0d726ae8598f58ae7fd
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2021-05-13 15:49:31 +0100

    Change to LocalizedStringKey

--- Sources/AFTextEditor/TextEditorField.swift
+++ Sources/AFTextEditor/TextEditorField.swift
@@ -8,23 +8,22 @@
 import SwiftUI
 
 public struct TextEditorField: View {
-    let placeHolder: String
+    let placeHolder: LocalizedStringKey
     let borderColor: Color
     let borderLine: CGFloat
     let textColor: Color
     let backgroundColor: Color
     @Binding var text: String
 
-
     /// Creates a text field with a text label in which users can enter and edit long-form text.
     /// - Parameters:
-    ///   - placeHolder: Required - String for the placeholder
+    ///   - placeHolder: Required - LocalizedStringKey for the placeholder
     ///   - borderColor: Color for the border - Default to match.textFieldStyle(RoundedBorderTextFieldStyle())
     ///   - borderLine: Line Width of Border - Default to match.textFieldStyle(RoundedBorderTextFieldStyle())
     ///   - textColor: Placeholder Text Color - Default to match.textFieldStyle(RoundedBorderTextFieldStyle())
     ///   - backgroundColor: Color for the background of text editor - Default is .clear
     ///   - text: Required - Binding text of user input
-    public init(_ placeHolder: String,
+    public init(_ placeHolder: LocalizedStringKey,
          borderColor: Color = Color.secondary.opacity(0.2),
          borderLine: CGFloat = 1,
          textColor: Color = Color(UIColor.placeholderText),
